f1264f3b5c92c24fa4e37a85500b54d85b074e92
prokarma <prokarma@nyadlapati.pkrmsezdc.com>
2018-11-07 02:04:33 -0800

    Initial commit

---
+++ SparksConfig/src/main/java/com/example/demo/SparksConfigApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparksConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparksConfigApplication.class, args);
+	}
+}
