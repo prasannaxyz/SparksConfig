f8a257bfbfc4141a5d6fb5919b8392bf0c67188c
prokarma <prokarma@nyadlapati.pkrmsezdc.com>
2018-11-07 07:40:29 -0800

    c

--- SparksConfig/src/main/java/com/example/demo/SparksConfigApplication.java
+++ SparksConfig/src/main/java/com/example/demo/SparksConfigApplication.java
@@ -1,5 +1,9 @@
 package com.example.demo;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,14 +14,19 @@ public class SparksConfigApplication implements CommandLineRunner{
 	private static String cluster = System.getProperty("cluster");
 	private static String keyspace = System.getProperty("keyspace");
 	
+	@Autowired
+	ApplicationArguments appArgs;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SparksConfigApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("cluster:"+cluster);
-		System.out.println("keyspace:"+keyspace);
+		List<String> cluster = appArgs.getOptionValues("cluster");
+		System.out.println("cluster:"+cluster.get(0));
+		List<String> keyspace = appArgs.getOptionValues("keyspace");
+		System.out.println("keyspace:"+keyspace.get(0));
 		
 	}
 }
