9be5e08f69bf95c33f07427a89513d0e6f068f83
prokarma <prokarma@nyadlapati.pkrmsezdc.com>
2018-11-07 03:30:15 -0800

    commit

--- SparksConfig/src/main/java/com/example/demo/SparksConfigApplication.java
+++ SparksConfig/src/main/java/com/example/demo/SparksConfigApplication.java
@@ -1,13 +1,14 @@
 package com.example.demo;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SparksConfigApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
-
+		SpringApplication.run(SparksConfigApplication.class, args);
 	}
 
 	@Override
