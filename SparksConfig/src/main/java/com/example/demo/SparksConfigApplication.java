86785de35c42e5e4bec2261c839d7f556f92b527
prokarma <prokarma@nyadlapati.pkrmsezdc.com>
2018-11-07 07:24:01 -0800

    commit

--- SparksConfig/src/main/java/com/example/demo/SparksConfigApplication.java
+++ SparksConfig/src/main/java/com/example/demo/SparksConfigApplication.java
@@ -7,13 +7,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SparksConfigApplication implements CommandLineRunner{
 
+	private static String cluster = System.getProperty("cluster");
+	private static String keyspace = System.getProperty("keyspace");
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SparksConfigApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
+		System.out.println("cluster:"+cluster);
+		System.out.println("keyspace:"+keyspace);
 		
 	}
 }
