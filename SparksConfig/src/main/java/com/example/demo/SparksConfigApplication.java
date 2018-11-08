11ed76e92ce09543be40d318706834b308938cfb
prokarma <prokarma@nyadlapati.pkrmsezdc.com>
2018-11-07 18:06:17 -0800

    Sparks configuration jenkins and local

--- SparksConfig/src/main/java/com/example/demo/SparksConfigApplication.java
+++ SparksConfig/src/main/java/com/example/demo/SparksConfigApplication.java
@@ -1,35 +1,68 @@
 package com.example.demo;
 
+import java.io.IOException;
+import java.util.Properties;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.vo.GlobalProperties;
+
 @SpringBootApplication
-public class SparksConfigApplication implements CommandLineRunner{
+public class SparksConfigApplication implements CommandLineRunner {
 
 	private static String cluster = System.getenv("cluster");
 	private static String keyspace = System.getenv("keyspace");
 	private static String readTimeout = System.getenv("readTimeout");
 	private static String compression = System.getenv("compression");
 	private static String connectionPerExecutorMax = System.getenv("connectionPerExecutorMax");
-	
+
 	@Autowired
 	ApplicationArguments appArgs;
-	
+
+	private static GlobalProperties global;
+
 	public static void main(String[] args) {
 		SpringApplication.run(SparksConfigApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("cluster:"+cluster);
-		System.out.println("keyspace:"+keyspace);
-		System.out.println("readTimeout:"+readTimeout);
-		System.out.println("compression:"+compression);
-		System.out.println("connectionPerExecutorMax:"+connectionPerExecutorMax);
+		System.out.println("**Jenkins Parameters*********");
+		System.out.println("cluster:" + cluster);
+		System.out.println("keyspace:" + keyspace);
+		System.out.println("readTimeout:" + readTimeout);
+		System.out.println("compression:" + compression);
+		System.out.println("connectionPerExecutorMax:" + connectionPerExecutorMax);
 		
-		
+		System.out.println("**Local Parameters*********");
+		System.out.println("cluster:" + global.getCluster());
+		System.out.println("keyspace:" + global.getKeyspace());
+		System.out.println("readTimeout:" + global.getReadTimeout());
+		System.out.println("compression:" + global.getCompression());
+		System.out.println("connectionPerExecutorMax:" + global.getConnectionPerExecutorMax());
+
 	}
+
+	@Autowired
+	public void setGlobal(GlobalProperties global) {
+		this.global = global;
+	}
+
+	public static Properties getSparkConfigProperties() {
+		Properties properties = new Properties();
+
+		try {
+			properties.load(
+					SparksConfigApplication.class.getClassLoader().getResourceAsStream("sparks-config.properties"));
+		} catch (IOException e) {
+			e.getMessage();
+		}
+
+		return properties;
+	}
+
 }
---
+++ SparksConfig/src/main/java/com/example/vo/GlobalProperties.java
@@ -0,0 +1,66 @@
+package com.example.vo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource("classpath:sparks-config.properties")
+public class GlobalProperties {
+
+	@Value("${cluster}")
+	private String cluster;
+	
+	@Value("${readTimeout}")
+	private String readTimeout;
+	
+	@Value("${keyspace}")
+	private String keyspace;
+	
+	@Value("${connectionPerExecutorMax}")
+	private String connectionPerExecutorMax;
+	
+	@Value("${compression}")
+	private String compression;
+
+	public String getCluster() {
+		return cluster;
+	}
+
+	public void setCluster(String cluster) {
+		this.cluster = cluster;
+	}
+
+	public String getReadTimeout() {
+		return readTimeout;
+	}
+
+	public void setReadTimeout(String readTimeout) {
+		this.readTimeout = readTimeout;
+	}
+
+	public String getKeyspace() {
+		return keyspace;
+	}
+
+	public void setKeyspace(String keyspace) {
+		this.keyspace = keyspace;
+	}
+
+	public String getConnectionPerExecutorMax() {
+		return connectionPerExecutorMax;
+	}
+
+	public void setConnectionPerExecutorMax(String connectionPerExecutorMax) {
+		this.connectionPerExecutorMax = connectionPerExecutorMax;
+	}
+
+	public String getCompression() {
+		return compression;
+	}
+
+	public void setCompression(String compression) {
+		this.compression = compression;
+	}
+
+}
