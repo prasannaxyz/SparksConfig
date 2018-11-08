84551c12f37d290e55c79f16d7973f2651f5966d
prokarma <prokarma@nyadlapati.pkrmsezdc.com>
2018-11-07 17:40:08 -0800

    Reading sparks properties

--- SparksConfig/src/main/java/com/example/demo/SparksConfigApplication.java
+++ SparksConfig/src/main/java/com/example/demo/SparksConfigApplication.java
@@ -11,13 +11,14 @@ public class SparksConfigApplication implements CommandLineRunner{
 
 	private static String cluster = System.getenv("cluster");
 	private static String keyspace = System.getenv("keyspace");
+	private static String readTimeout = System.getenv("readTimeout");
+	private static String compression = System.getenv("compression");
+	private static String connectionPerExecutorMax = System.getenv("connectionPerExecutorMax");
 	
 	@Autowired
 	ApplicationArguments appArgs;
 	
 	public static void main(String[] args) {
-		System.out.println("cluster:"+cluster);
-		System.out.println("keyspace:"+keyspace);
 		SpringApplication.run(SparksConfigApplication.class, args);
 	}
 
@@ -25,6 +26,10 @@ public class SparksConfigApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		System.out.println("cluster:"+cluster);
 		System.out.println("keyspace:"+keyspace);
+		System.out.println("readTimeout:"+readTimeout);
+		System.out.println("compression:"+compression);
+		System.out.println("connectionPerExecutorMax:"+connectionPerExecutorMax);
+		
 		
 	}
 }
